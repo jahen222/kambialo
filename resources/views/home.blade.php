45e813df13904594674e7f410d9ba33ed97bd4ea
Henry Jaimes <henryjaimes.peli@gmail.com>
2020-06-26 16:40:08 -0400

    Slider like tinder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,79 +1,187 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link href="{{ asset('css/swipe.css') }}" rel="stylesheet">
+
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
-                  Buscar por Categorías
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
+          Buscar por Categorías
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Seleccione una categoría</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
                 </button>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Seleccione una categoría</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <form action="{{route('search.category')}}" method="POST">
-                        {{ csrf_field() }}
-                        <div class="modal-body">
-                          <?php foreach ($categories as $category): ?>
-                            <input type="radio" name="search" value="{{$category->id}}">
-                            <label for="search">{{$category->name}}</label><br>
-                          <?php endforeach; ?>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                          <button type="submit" class="btn btn-primary">Buscar</button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
+              </div>
+              <form action="{{route('search.category')}}" method="POST">
+                {{ csrf_field() }}
+                <div class="modal-body">
+                  <?php foreach ($categories as $category) : ?>
+                    <input type="radio" name="search" value="{{$category->id}}">
+                    <label for="search">{{$category->name}}</label><br>
+                  <?php endforeach; ?>
                 </div>
-
-                <div class="card-body" style="padding: 0px;">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <?php if (count($products)>0): ?>
-                      <div class="slideshow-container">
-                        <?php foreach ($products as $product): ?>
-                          <div class="mySlides">
-                            <a href="{{ route('product.show', $product->id) }}">
-                              <img src="/images/{{$product->cover_image}}" style="width:100%">
-                              <div class="text">{{$product->name}}</div>
-                            </a>
-                          </div>
-                        <?php endforeach; ?>
-                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                      </div>
-                      <img src="/images/fake_footer.png" style="width:100%">
-                    <?php else: ?>
-                      <div class="card-body">
-                        @if (session('status'))
-                          <div class="alert alert-success" role="alert">
-                              {{ session('status') }}
-                          </div>
-                        @endif
-
-                        No hay productos.
-                      </div>
-                    <?php endif; ?>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                  <button type="submit" class="btn btn-primary">Buscar</button>
                 </div>
+              </form>
             </div>
+          </div>
         </div>
+
+        <div class="card-body" style="padding: 0px;">
+          @if (session('status'))
+          <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+          </div>
+          @endif
+
+          <?php if (count($products) > 0) : ?>
+            <div class="slideshow-container">
+              <?php foreach ($products as $product) : ?>
+                <div class="mySlides">
+                  <a href="{{ route('product.show', $product->id) }}">
+                    <img src="/images/{{$product->cover_image}}" style="width:100%">
+                    <div class="text">{{$product->name}}</div>
+                  </a>
+                </div>
+              <?php endforeach; ?>
+              <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+              <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+            <img src="/images/fake_footer.png" style="width:100%">
+          <?php else : ?>
+            <div class="card-body">
+              @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+              @endif
+
+              No hay productos.
+            </div>
+          <?php endif; ?>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.js"></script>
+
 <script>
+  $('.js-lazyload').lazyload({
+    effect: 'fadeIn',
+    threshold: 50,
+  });
+
+  //Globals
+  var $topCard,
+    //deltaThreshold is the swipe distance from the initial place of the card
+    deltaThreshold = 100,
+    deltaX = 0;
+
+  function swipeEnded(event, direction, $card) {
+    var directionFactor,
+      transform;
+    //If the event has a type, then it is triggered from a button and has a given direction
+    if (event.type === 'click') {
+      directionFactor = direction === 'right' ? -1 : 1;
+    }
+    //If the event has a deltaX, then it is triggered from a gesture and has a calculated direction
+    else if (event.deltaX) {
+      directionFactor = event.deltaX >= 0 ? -1 : 1;
+    }
+
+    //If the threshold is reached or a trigger clicked, the card is thrown on a side and then disappear
+    if (event.deltaX && deltaX > deltaThreshold || event.deltaX && deltaX < -1 * deltaThreshold || direction) {
+      transform = 'translate(' + directionFactor * -100 + 'vw, 0) rotate(' + directionFactor * -5 + 'deg)';
+      $card
+        .delay(100)
+        .queue(function() {
+          $(this).css('transform', transform).dequeue();
+        })
+        .delay(300)
+        .queue(function() {
+          $(this).addClass('done').remove();
+        });
+
+      //Do something
+      console.log('Swipe done. \nCard:', $card, '\nDirection:', directionFactor);
+
+    }
+    //If the threshold isn't reached, the card goes back to its initial place
+    else {
+      transform = 'translate(0, 0) rotate(0)';
+      $card.css({
+        'transform': transform,
+      });
+    }
+  }
+
+  function swipeLeft(event, $card) {
+    var transform;
+    deltaX = event.deltaX;
+    transform = 'translate(' + deltaX * 0.8 + 'px, 0) rotate(5deg)';
+    //translate the card on swipe
+    $card.css({
+      'transform': transform,
+    });
+  }
+
+  function swipeRight(event, $card) {
+    var transform;
+    deltaX = event.deltaX;
+    transform = 'translate(' + deltaX * 0.8 + 'px, 0) rotate(-5deg)';
+    //translate the card on swipe
+    $card.css({
+      'transform': transform,
+    });
+  }
+
+  //Events
+  $('.js-swiping-card').each(function(index, element) {
+    var $card = $(element),
+      //Add hammer events on element
+      hammertime = new Hammer(element);
+
+    //Mobile gesture
+    hammertime.on('panleft swipeleft', function(event) {
+      swipeLeft(event, $card);
+    });
+    hammertime.on('panright swiperight', function(event) {
+      swipeRight(event, $card);
+    });
+    hammertime.on('panend', function(event) {
+      swipeEnded(event, false, $card);
+    });
+  });
+
+  //Btn controls
+  $('.js-left-trigger').on('click', function(event) {
+    var $topCard = $('.js-swiping-card').last();
+    swipeEnded(event, 'left', $topCard);
+  });
+  $('.js-right-trigger').on('click', function(event) {
+    console.log('sapo');
+    var $topCard = $('.js-swiping-card').last();
+    swipeEnded(event, 'right', $topCard);
+  });
+</script>
+
+<script type="application/javascript">
   var slideIndex = 1;
   showSlides(slideIndex);
 
@@ -90,15 +198,20 @@
     var slides = document.getElementsByClassName("mySlides");
     console.log(slides);
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
   }
 </script>
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,89 +1,256 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="{{ asset('css/swipe.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Inicio</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Registro</a>
-                        @endif
-                    @endauth
-                </div>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Inicio</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Registro</a>
             @endif
+            @endauth
+        </div>
+        @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Kambialo
-                </div>
+        <div class="container">
+            <div class="stack">
+                <div class="final-message">No more kitten :'(</div>
+                <ul>
+                    <li>
+                        <div id="card10" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/300/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card9" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/400/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="car8" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/450/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card7" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/800/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card6" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/100/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card5" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/400/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card4" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/150/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card3" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="http://www.escaledigitale.com/im/team/anita@2x.png"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card2" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/600/200"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="card1" class="card js-swiping-card">
+                            <div class="card-illustration js-lazyload" data-original="https://placekitten.com/g/400/200"></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="btn-controls text-center">
+                <button type="button" class="btn btn-danger js-left-trigger">
+                    Ugly kitten
+                </button>
+                <button type="button" class="btn btn-primary js-right-trigger">
+                    Atomic kitten
+                </button>
             </div>
         </div>
-    </body>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.js"></script>
+    <script>
+        $('.js-lazyload').lazyload({
+            effect: 'fadeIn',
+            threshold: 50,
+        });
+
+        //Globals
+        var $topCard,
+            //deltaThreshold is the swipe distance from the initial place of the card
+            deltaThreshold = 100,
+            deltaX = 0;
+
+        function swipeEnded(event, direction, $card) {
+            var directionFactor,
+                transform;
+            //If the event has a type, then it is triggered from a button and has a given direction
+            if (event.type === 'click') {
+                directionFactor = direction === 'right' ? -1 : 1;
+            }
+            //If the event has a deltaX, then it is triggered from a gesture and has a calculated direction
+            else if (event.deltaX) {
+                directionFactor = event.deltaX >= 0 ? -1 : 1;
+            }
+
+            //If the threshold is reached or a trigger clicked, the card is thrown on a side and then disappear
+            if (event.deltaX && deltaX > deltaThreshold || event.deltaX && deltaX < -1 * deltaThreshold || direction) {
+                transform = 'translate(' + directionFactor * -100 + 'vw, 0) rotate(' + directionFactor * -5 + 'deg)';
+                $card
+                    .delay(100)
+                    .queue(function() {
+                        $(this).css('transform', transform).dequeue();
+                    })
+                    .delay(300)
+                    .queue(function() {
+                        $(this).addClass('done').remove();
+                    });
+
+                //Do something
+                console.log('Swipe done. \nCard:', $card, '\nDirection:', directionFactor);
+
+            }
+            //If the threshold isn't reached, the card goes back to its initial place
+            else {
+                transform = 'translate(0, 0) rotate(0)';
+                $card.css({
+                    'transform': transform,
+                });
+            }
+        }
+
+        function swipeLeft(event, $card) {
+            var transform;
+            deltaX = event.deltaX;
+            transform = 'translate(' + deltaX * 0.8 + 'px, 0) rotate(5deg)';
+            //translate the card on swipe
+            $card.css({
+                'transform': transform,
+            });
+        }
+
+        function swipeRight(event, $card) {
+            var transform;
+            deltaX = event.deltaX;
+            transform = 'translate(' + deltaX * 0.8 + 'px, 0) rotate(-5deg)';
+            //translate the card on swipe
+            $card.css({
+                'transform': transform,
+            });
+        }
+
+        //Events
+        $('.js-swiping-card').each(function(index, element) {
+            var $card = $(element),
+                //Add hammer events on element
+                hammertime = new Hammer(element);
+
+            //Mobile gesture
+            hammertime.on('panleft swipeleft', function(event) {
+                swipeLeft(event, $card);
+            });
+            hammertime.on('panright swiperight', function(event) {
+                swipeRight(event, $card);
+            });
+            hammertime.on('panend', function(event) {
+                swipeEnded(event, false, $card);
+            });
+        });
+
+        //Btn controls
+        $('.js-left-trigger').on('click', function(event) {
+            var $topCard = $('.js-swiping-card').last();
+            swipeEnded(event, 'left', $topCard);
+        });
+        $('.js-right-trigger').on('click', function(event) {
+            var $topCard = $('.js-swiping-card').last();
+            swipeEnded(event, 'right', $topCard);
+        });
+    </script>
+</body>
+
 </html>
