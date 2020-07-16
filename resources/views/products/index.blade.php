090ed0a91192b6c9aad1f95cb009c320e183ff84
Alessandro Arenas <alessandroarena.15@gmail.com>
2020-07-16 16:29:17 -0400

    Color

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,10 +3,39 @@
 @section('content')
 
 <style>
+.mdc-fab:not(.mdc-fab--extended) {
+    border-radius: 50%;
+}
 .app-fab--absolute {
   position: fixed;
   bottom: 1rem;
   right: 1rem;
+  background-color: var(--mdc-theme-primary, #6200ee);
+}
+
+.mdc-fab {
+    box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),0px 6px 10px 0px rgba(0, 0, 0, 0.14),0px 1px 18px 0px rgba(0,0,0,.12);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    width: 56px;
+    height: 56px;
+    padding: 0;
+    border: none;
+    fill: currentColor;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    overflow: hidden;
+    transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1),opacity 15ms linear 30ms,transform 270ms 0ms cubic-bezier(0, 0, 0.2, 1);
+    background-color: #018786;
+    color: #fff;
+    color: var(--mdc-theme-on-secondary, #fff);
 }
 
 @media(min-width: 1024px) {
