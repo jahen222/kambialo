85b230dd7acfb59d275cd6816d6151758dedac19
Johan Quintero <jrquintero@gmail.com>
2020-08-11 18:59:47 -0400

    Desactivando Middleware

--- routes/web.php
+++ routes/web.php
@@ -31,22 +31,22 @@ Route::post('webpay/confirmation', 'WebpayController@confirmation')->name('webpa
 
 //Route::middleware(['auth'])->group(function () {
     // Products
-    Route::get('products', 'ProductController@index')->name('products.index')->middleware('permission:index');
-    Route::get('product/create', 'ProductController@create')->name('product.create')->middleware('permission:create');
-    Route::post('product/store', 'ProductController@store')->name('product.store')->middleware('permission:store');
-    Route::get('product/{role}', 'ProductController@show')->name('product.show')->middleware('permission:show');
-    Route::get('product/{role}/edit', 'ProductController@edit')->name('product.edit')->middleware('permission:edit');
-    Route::put('product/{role}', 'ProductController@update')->name('product.update')->middleware('permission:update');
-    Route::delete('product/{role}', 'ProductController@destroy')->name('product.destroy')->middleware('permission:destroy');
+    Route::get('products', 'ProductController@index')->name('products.index');//->middleware('permission:index');
+    Route::get('product/create', 'ProductController@create')->name('product.create');//->middleware('permission:create');
+    Route::post('product/store', 'ProductController@store')->name('product.store');//->middleware('permission:store');
+    Route::get('product/{role}', 'ProductController@show')->name('product.show');//->middleware('permission:show');
+    Route::get('product/{role}/edit', 'ProductController@edit')->name('product.edit');//->middleware('permission:edit');
+    Route::put('product/{role}', 'ProductController@update')->name('product.update');//->middleware('permission:update');
+    Route::delete('product/{role}', 'ProductController@destroy')->name('product.destroy');//->middleware('permission:destroy');
     // Category
     Route::post('search/category', 'HomeController@searchCategory')->name('search.category');
     // Favorites
-    Route::get('favorites', 'FavoriteController@index')->name('favorites.index')->middleware('permission:index');
-    Route::post('favorite/store', 'FavoriteController@store')->name('favorite.store')->middleware('permission:store');
+    Route::get('favorites', 'FavoriteController@index')->name('favorites.index');//->middleware('permission:index');
+    Route::post('favorite/store', 'FavoriteController@store')->name('favorite.store');//->middleware('permission:store');
     // Matches
-    Route::get('matches', 'MatchController@index')->name('matches.index')->middleware('permission:index');
-    Route::get('match/{role}', 'MatchController@show')->name('match.show')->middleware('permission:show');
+    Route::get('matches', 'MatchController@index')->name('matches.index');//->middleware('permission:index');
+    Route::get('match/{role}', 'MatchController@show')->name('match.show');//->middleware('permission:show');
 
     // Favorites
-    Route::post('match/store', 'FavoriteController@storeHome')->name('favorite_home.store')->middleware('permission:store');
+    Route::post('match/store', 'FavoriteController@storeHome')->name('favorite_home.store');//->middleware('permission:store');
 //});
