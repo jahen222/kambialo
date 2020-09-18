ac837f7fbcc0fde7666576c0b055ef4217860b57
armandoq <armandojquinterol@gmail.com>
2020-09-18 16:49:48 -0400

    Mejora. mensaje de validacion de formularios en español

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -29,6 +29,6 @@
 @if ($errors->any())
 <div class="alert alert-danger">
     <button type="button" class="close" data-dismiss="alert">×</button>    
-    Please check the form below for errors
+	Por favor, verifique los errores en el formulario a continuación 
 </div>
 @endif
