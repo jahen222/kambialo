b3388999d3fad81805c7c67f5b37c1173e956e31
armandoq <armandojquinterol@gmail.com>
2021-01-04 15:08:02 -0400

    Tamaño iconos menu inferior vitrinea

--- resources/views/showcase/index.blade.php
+++ resources/views/showcase/index.blade.php
@@ -4,11 +4,11 @@
     <card-component v-bind:category="'{{ request('category') }}'" v-bind:url="'{{ url()->to('product') }}'" v-bind:public="'{{ config('constants.publicUrl') }}'"></card-component>
 
     <div class="nav-bottom" style="margin-bottom: 10px; z-index: 1000;">
-        <div><a href="{{ url()->to('/') }}"><img style="max-height: 24px;" src="{{ asset('assets/img/iconos_app-06.png') }}"alt=""></a></div>
+        <div><a href="{{ url()->to('/') }}"><img style="height: 30px;" src="{{ asset('assets/img/iconos_app-06.png') }}"alt=""></a></div>
         <div class="dropdown show">
             <a style="color:white;" href="#" onclick="resetNotification()" role="button" id="dropdownMenuLink"
                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img style="max-height: 24px;"
+                <img style="height: 30px;"
                      src="{{ asset('assets/img/iconos_app-09.png') }}" alt="">
                 @if($user = auth()->user())
                     <span style="{{ count($user->unreadNotifications) ?: 'display:none;' }}" class="notification"
@@ -25,9 +25,9 @@
                 </div>
             @endif
         </div>
-        <div><a href="{{ route('product.create') }}"><img style="max-height: 24px;" src="{{ asset('assets/img/iconos_app-08.png') }}" alt=""></a></div>
-        <!--<div><a href="{{ route('matches.index') }}"><img style="max-height: 24px;" src="{{ asset('assets/img/iconos_app-07.png') }}" alt=""></a></div>-->
-        <div><a href="{{ route('user.edit') }}"><img style="max-height: 24px;" src="{{ asset('assets/img/iconos_app-10.png') }}" alt=""></a></div>
+        <div><a href="{{ route('product.create') }}"><img style="height: 30px;" src="{{ asset('assets/img/iconos_app-08.png') }}" alt=""></a></div>
+        <!--<div><a href="{{ route('matches.index') }}"><img style="height: 30px;" src="{{ asset('assets/img/iconos_app-07.png') }}" alt=""></a></div>-->
+        <div><a href="{{ route('user.edit') }}"><img style="height: 30px;" src="{{ asset('assets/img/iconos_app-10.png') }}" alt=""></a></div>
     </div>
 @endsection
 
